dc013b5e430f1b190ddd1f537da4e196eeaf0a5e
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2022-02-06 14:34:30 -0200

    Tipos de dados primitivos

---
+++ aula04.py
@@ -0,0 +1,20 @@
+
+
+#TIPOS DE DADOS 
+#str - string - textos 'Assim' "Assim"
+#int - inteiro - 123456 0 -10 -20 -50 -60 -15000 1500
+#float - real/ponto flutuante - 10.50 1.5 -10.10 -50.93 0.0
+#bool - booleano/logico - True/False 10 == 10
+
+
+# String: nome
+print('luiz', type('luiz'))
+
+# Idade: int
+print('28', type('28'))
+
+# Altura: float
+print('1.80', type('1.80'))
+
+# E maior de idade 10 > 20 
+print(18 < 0, type(18 < 0))
