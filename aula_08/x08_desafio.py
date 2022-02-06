ab0ebd5001d4f81e8a3e7c6cd0ca30343f9e5ff1
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2022-02-06 19:34:34 -0200

    Desafio

---
+++ aula_08/x08_desafio.py
@@ -0,0 +1,13 @@
+nome = 'Luiz'
+idade = 32
+altura = 1.8
+peso = 80
+imc = peso / altura ** 2
+ano_atual = 2022
+nasceu = ano_atual - idade
+
+print(f'{nome}, tem {idade} anos, {altura}, de altura e pesa {peso}kg.')
+print(f'O IMC de {nome} e {imc:.2f}.')
+print(f'{nome}, nasceu em {nasceu}')
+
+
