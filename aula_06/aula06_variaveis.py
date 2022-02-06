8d534fae9e1b4313518c5bed46a07476bd09ee04
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2022-02-06 19:01:15 -0200

    Aula Basica, Calculando IMC

---
+++ aula_06/aula06_variaveis.py
@@ -0,0 +1,21 @@
+# Iniciar com letra, pode conter numeros, separar _, letras minusculas
+
+#nome = 'leandro'
+#print('nome', type('nome'))
+
+nome = 'Leonardo'
+idade = 28
+altura = 1.80
+e_maior = idade > 18
+peso = 80
+imc = peso / altura ** 2
+
+
+
+print(nome, 'tem', idade, 'anos de idade e seu imc e:' , imc)
+
+
+#print('Nome:', nome)
+#print('Idade:', idade)
+#print('altura:', altura)
+#print('E maior :', e_maior)
